23e2ab3319c55221d674d60d8e507b182cefff3b
gn0me5 <antonina.yastrebova@gmail.com>
2026-05-16 15:53:33 +0300

    Create cell_towers.py

---
+++ cell_towers.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+cols = ['radio','mcc','net','area','cell','unit','lon','lat','range','samples','changeable','created','updated','averageSignal']
+cell_tower_data = pd.read_csv('/content/244.csv.gz', compression='gzip', 
+                                 header=None, names = cols, index_col=False, sep=',', quotechar='"')
+
+cell_tower_data
